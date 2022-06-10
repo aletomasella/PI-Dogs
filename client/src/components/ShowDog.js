e9e784c586b83377020f2e8333557a6b6c30d169
Alejandro Tomasella <aletomasella09@gmail.com>
2022-06-10 09:36:11 -0300

    Minor Change

--- client/src/components/ShowDog.js
+++ client/src/components/ShowDog.js
@@ -33,6 +33,7 @@ const ShowDog = () => {
           alignContent: "space-between",
           alignItems: "center",
           color: "white",
+          fontWeight: "600",
         }}
       >
         {dogs &&
